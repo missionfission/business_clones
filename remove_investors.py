01f476d184d36cf20a776a1b107f0bca358bde84
Khushal <iaskhushal@gmail.com>
2024-12-17 04:09:16 +0530

    l

--- remove_investors.py
+++ remove_investors.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Read the CSV file
-df = pd.read_csv('CB-Insights_Global-Unicorn-Club_2024.csv')
+df = pd.read_csv('2023-02-27-yc-companies.csv')
 print("Column names:", df.columns.tolist())
 
 # Drop the 'Select Investors' column
-df = df.drop('Select Investors', axis=1)
+df = df.drop('long_description', axis=1)
 
 # Save the modified dataframe to a new CSV file
-df.to_csv('CB-Insights_Global-Unicorn-Club_2024_no_investors.csv', index=False) 
+df.to_csv('yc-companies.csv', index=False) 
